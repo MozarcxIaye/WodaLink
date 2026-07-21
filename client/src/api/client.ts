f475382003309d15eadd70614f9e0686c7b8c2b7
MozarcxIaye <mozarcxiaye@gmail.com>
2026-07-21 09:58:34 +0545

    Fix: fixed api url

--- client/src/api/client.ts
+++ client/src/api/client.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
-// const API_URL = 'https://wodalink.onrender.com/'; 
-const API_URL = 'http://localhost:3001'; 
+const API_URL = 'https://wodalink.onrender.com/'; 
+// const API_URL = 'http://localhost:3001'; 
 
 
 export const apiClient = axios.create({
